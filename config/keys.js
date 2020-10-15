a73c91870c8a95052d71c8d71b7f41104b8a6158
omkarmanchekar24 <33656332+omkarmanchekar24@users.noreply.github.com>
2020-10-15 15:49:25 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -3,7 +3,6 @@ module.exports = {
   mongoURI:
     "mongodb+srv://omkarmanchekar24:contextSuubot@24@suubot.gcg4n.mongodb.net/suubot?retryWrites=true&w=majority",
   secretOrKey: "secret",
-  ip: "http://192.168.0.10:5000",
   messageQueue:
-    "https://sqs.ap-south-1.amazonaws.com/865171056842/suubot-message-queue",
+    "",
 };
