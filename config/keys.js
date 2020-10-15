e4744e4fe8d53f27a1559cba67b50e02318c1008
omkarmanchekar24 <33656332+omkarmanchekar24@users.noreply.github.com>
2020-10-15 16:02:50 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,6 @@
 module.exports = {
-  //mongoURI: "mongodb://127.0.0.1:27017/suubot",
   mongoURI:
-    "mongodb+srv://omkarmanchekar24:contextSuubot@24@suubot.gcg4n.mongodb.net/suubot?retryWrites=true&w=majority",
+    "mongodb+srv://omkar24:444444@devcluster.0uilj.mongodb.net/DevCluster?retryWrites=true&w=majority",
   secretOrKey: "secret",
   messageQueue:
     "",
